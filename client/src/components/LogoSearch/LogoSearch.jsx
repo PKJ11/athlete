152433a20251403bb9fd903df9e700933fc7234d
PKJ11 <108004611+PKJ11@users.noreply.github.com>
2024-03-16 09:54:08 +0530

    c11

--- client/src/components/LogoSearch/LogoSearch.jsx
+++ client/src/components/LogoSearch/LogoSearch.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Logo from "../../img/logo.png";
+import Logo from "../../img/ac.jpeg";
 import './LogoSearch.css'
 import { UilSearch } from '@iconscout/react-unicons'
 const LogoSearch = () => {
   return (
     <div className="LogoSearch">
-      <img src={Logo} alt="" />
+      <img src={Logo} alt="" style={{ width: "50px", height: "50px" }} />
       <div className="Search">
           <input type="text" placeholder="#Explore"/>
           <div className="s-icon">
